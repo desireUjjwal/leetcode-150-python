bcc3e4755009e6abca1e2442dff45daafda1a9df
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-18 13:14:10 +0530

    Update 13_ProductOfArrayExceptSelf.py

--- 01_ArrayString/13_ProductOfArrayExceptSelf.py
+++ 01_ArrayString/13_ProductOfArrayExceptSelf.py
@@ -19,3 +19,17 @@ class Solution:
         for i in range(0, n):
             ans[i] = prefix[i] * postfix[i]
         return ans
+# Space Optimized
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [1] * n
+        for i in range(1, n):
+            ans[i] = ans[i-1] * nums[i-1]
+        postfix = 1
+        for i in range(n-2, -1, -1):
+            postfix *= nums[i+1]
+            ans[i] = ans[i] * postfix
+        return ans
+
+        

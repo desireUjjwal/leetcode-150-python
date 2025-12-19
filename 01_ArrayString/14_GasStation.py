66ed854ff90e64cefb89d1d730f74d15488d11be
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-19 18:39:43 +0530

    Create 14_GasStation.py

---
+++ 01_ArrayString/14_GasStation.py
@@ -0,0 +1,27 @@
+# 134. Gas Station
+
+# There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+# You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station.
+# You begin the journey with an empty tank at one of the gas stations.
+
+# Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1.
+# If there exists a solution, it is guaranteed to be unique.
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        total = 0
+        ind = 0
+        for i in range(len(gas)):
+            total += gas[i] - cost[i]
+            if total < 0:
+                total = 0
+                ind = i + 1
+        return ind
+        
+        
+
+# check first that total gas should be greater than total cost then there must be a solution
+# [-2,-2,-2,3,3] just pick and index and check can we cross the array if yes that index is the answer.

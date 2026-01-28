a9b558163b61eea4e19743774eb40cb343973d18
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2026-01-29 00:42:29 +0530

    Create 10_SumRootToLeafNumbers.py

---
+++ 09_BinaryTreeGeneral/10_SumRootToLeafNumbers.py
@@ -0,0 +1,39 @@
+# 129. Sum Root to Leaf Numbers
+
+# You are given the root of a binary tree containing digits from 0 to 9 only.
+
+# Each root-to-leaf path in the tree represents a number.
+
+# For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+# Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+# A leaf node is a node with no children.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def helper(self, root: Optional[TreeNode], num: str, res: list[int]) -> None:
+        if not root.left and not root.right:
+            num += str(root.val)
+            res.append(int(num))
+            num = num[:-1]
+            return
+        num += str(root.val)
+        if(root.left):
+            self.helper(root.left, num, res)
+        if(root.right):
+            self.helper(root.right, num, res)
+            num = num[:-1]
+
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        num = ""
+        res = []
+        totalSum = 0
+        self.helper(root, num, res)
+        for i in range(len(res)):
+            totalSum += res[i]
+        return totalSum
